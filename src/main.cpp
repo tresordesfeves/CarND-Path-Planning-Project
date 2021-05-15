baf3fc0cd6b006837f1196a9a019876d820c84b0
Philippe Rougier <rougierphilippe@hotmail.com>
2021-05-14 22:20:50 -0700

    fix error on dist_inc

--- src/main.cpp
+++ src/main.cpp
@@ -103,15 +103,14 @@ int main() {
           //* TODO(1): define a path made up of (x,y) points that the car will visit
           double next_s, next_d;
           double dist_inc = 0.5;
+          vector<double>car_XY;
           for (int i = 0; i < 50; ++i) 
             {
-              next_s= car_s*(i+1);
+              next_s= car_s+((i+1)*dist_inc);
               next_d= car_d+6;
-
-              car_x, car_y = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x,map_waypoints_y);
-                              
-              next_x_vals.push_back(car_x);
-              next_y_vals.push_back(car_y);
+              car_XY=getXY(next_s,next_d,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+              next_x_vals.push_back(car_XY[0]);
+              next_y_vals.push_back(car_XY[1]);
             }
            //* END(1)                              
 
